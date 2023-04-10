d75d23d5ff0ae6f69e1c5af6d99b9e4412867a68
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2023-04-10 15:48:57 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,135 @@
+let formElement = document.querySelector(".js-form");
+let amountElement = document.querySelector(".js-amount");
+let currencyToElement = document.querySelector(".js-currencyTo");
+let currencyFromElement = document.querySelector(".js-currencyFrom");
+let resultElement = document.querySelector(".js-result");
+let exchangeRateElement = document.querySelector(".js-exchangeRate");
+
+let ratePLN = 1.00;
+let rateUSD = 4.3981;
+let rateEUR = 4.6871;
+let rateGBP = 5.2836;
+let rateCHF = 4.7156;
+let rateAUD = 2.9292;
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let amount = Number(amountElement.value);
+    let currencyTo = currencyToElement.value;
+    let currencyFrom = currencyFromElement.value;
+
+    let result;
+
+    switch (currencyFrom) {
+        case "USD":
+            part = amount * rateUSD;
+            break;
+
+        case "EUR":
+            part = amount * rateEUR;
+            break;
+
+        case "GBP":
+            part = amount * rateGBP;
+            break;
+
+        case "CHF":
+            part = amount * rateCHF;
+            break;
+
+        case "AUD":
+            part = amount * rateAUD;
+            break;
+
+            case "PLN":
+            part = amount * ratePLN;
+            break;
+    }
+
+    switch (currencyTo) {
+        case "USD":
+            result = part / rateUSD;
+            break;
+
+        case "EUR":
+            result = part / rateEUR;
+            break;
+
+        case "GBP":
+            result = part / rateGBP;
+            break;
+
+        case "CHF":
+            result = part / rateCHF;
+            break;
+
+        case "AUD":
+            result = part / rateAUD;
+            break;
+
+            case "PLN":
+            result = part / ratePLN;
+            break;
+    }
+   
+    resultElement.innerHTML = `${amount.toFixed(2)} ${currencyFrom} = <strong>${result.toFixed(2)} ${currencyTo}</strong>`;
+});
+
+formElement.addEventListener("input", () => {
+    let currencyTo = currencyToElement.value;
+    let currencyFrom = currencyFromElement.value;
+
+    switch (currencyFrom) {
+        case "USD":
+            rate = rateUSD;
+            break;
+        case "EUR":
+            rate = rateEUR;
+            break;
+        case "GBP":
+            rate = rateGBP;
+            break;
+        case "CHF":
+            rate = rateCHF;
+            break;
+        case "AUD":
+            rate = rateAUD;
+            break;
+            case "PLN":
+            rate = ratePLN;
+            break;
+    }
+
+    switch (currencyTo) {
+        case "USD":
+            result = rate / rateUSD;
+            break;
+        case "EUR":
+            result = rate / rateEUR;
+            break;
+        case "GBP":
+            result = rate / rateGBP;
+            break;
+        case "CHF":
+            result = rate / rateCHF;
+            break;
+        case "AUD":
+            result = rate / rateAUD;
+            break;
+            case "PLN":
+            result = rate / ratePLN;
+            break;
+    }
+
+    if (currencyFrom === currencyTo) {
+        exchangeRateElement.innerHTML = `1 ${currencyFrom} = ${result.toFixed(0)} ${currencyTo}`;
+    } else if (currencyFrom !== currencyTo) {
+    exchangeRateElement.innerHTML = `1 ${currencyFrom} = ${result.toFixed(4)} ${currencyTo}`;
+    }
+});
+
+formElement.addEventListener ("reset", (event) => {
+resultElement.innerText = "";
+exchangeRateElement.innerText = "";
+
+});
